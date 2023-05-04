6868a3ff02a2c378dd3bc3432dcfc3fea61d415e
kondapalli venkat narayana <59765476+venkatnarayana099@users.noreply.github.com>
2023-05-04 23:28:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,9 +33,7 @@ try:
    else:
     #import requests
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice )
-    # write your own comment -what does the next line do? 
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # write your own comment - what does this do?
     streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
